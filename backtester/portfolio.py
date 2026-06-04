879176742983f80039a1158cabb4ef1f93b2256a
chengq220 <chengqiu220@gmail.com>
2026-06-03 22:33:43 -0700

    update portfolio

--- backtester/portfolio.py
+++ backtester/portfolio.py
@@ -1,7 +1,10 @@
 from backtester.event import OrderEvent
 class Portfolio():
-    def __init__(self):
-        pass
-
+    def __init__(self, inital_cash = 10000):
+        self.free_cash = inital_cash
+        self.position = {}
+        self.trade_log = []
+        self.portfolio_history = []
+        
     def handle_signal(self, event):
         return 0
